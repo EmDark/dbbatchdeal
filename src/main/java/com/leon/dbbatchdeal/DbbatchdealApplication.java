92ff494607105e82222d4469e1b00aa8607b6640
Emdark <324698426@qq.com>
2022-08-25 10:00:32 +0800

    init

---
+++ src/main/java/com/leon/dbbatchdeal/DbbatchdealApplication.java
@@ -0,0 +1,13 @@
+package com.leon.dbbatchdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbbatchdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbbatchdealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leon/dbbatchdeal/DbbatchdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.dbbatchdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbbatchdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
